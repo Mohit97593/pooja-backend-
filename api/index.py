4280bee5c0b96efc0a55cf7e8103da2c21cdd72e
Mohit97593 <mohitanshu7800@gmail.com>
2026-05-04 12:01:29 +0530

    Master fix for pkg_resources

--- api/index.py
+++ api/index.py
@@ -1,9 +1,17 @@
+import sys
+import types
+
+# Master Fix for pkg_resources missing in Python 3.12 on Vercel
 try:
-    import setuptools
+    import pkg_resources
 except ImportError:
-    pass
+    # Create a dummy pkg_resources module to satisfy drf_yasg
+    pkg_res = types.ModuleType('pkg_resources')
+    pkg_res.DistributionNotFound = type('DistributionNotFound', (Exception,), {})
+    pkg_res.get_distribution = lambda x: types.SimpleNamespace(version='0.0.0')
+    sys.modules['pkg_resources'] = pkg_res
 
 from nxtbn.wsgi import application
 
-# This is the entry point for Vercel - Version 2
+# This is the entry point for Vercel
 app = application
